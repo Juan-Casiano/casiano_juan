f4c71590cd044a04c5650b19ab4fca60767cb78f
Juan-Casiano <juan_3127240893@utd.edu.mx>
2026-01-27 20:41:29 -0600

    Cambie el texto por el de la UTD

--- src/App.js
+++ src/App.js
@@ -1,21 +1,21 @@
-import logo from './logo.svg';
+import profile from './profile.png';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src={profile} className="App-logo" alt="profile" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+           <code>Soy un estudiante de la UTD y este es mi hola mundo en react</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://utd.edu.mx"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          ACCEDER A LA UTD
         </a>
       </header>
     </div>
