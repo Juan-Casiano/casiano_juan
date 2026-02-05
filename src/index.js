455473275ab7e7f5e3bb5e86629a2a0a617dd282
Juan-Casiano <juan_3127240893@utd.edu.mx>
2026-02-04 18:43:28 -0600

    Segunda pagina de react con mi linked in

--- src/index.js
+++ src/index.js
@@ -3,8 +3,6 @@ import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import Componente_david from './Componente_david';
-import Calculadora from './Calculadora';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
